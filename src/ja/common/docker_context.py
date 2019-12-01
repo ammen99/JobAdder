27a3768c39b3455e436328d3833b652fc5575172
Ilia Bozhinov <ammen99@gmail.com>
2019-12-01 17:52:37 +0100

    common: add docker_context.DockerConstraints

--- src/ja/common/docker_context.py
+++ src/ja/common/docker_context.py
@@ -50,3 +50,36 @@ class IDockerContext(ABC):
         '''
         @return A list of all mount points for the job.
         '''
+
+
+class DockerConstraints:
+    '''
+    A list of constraints of the docker container.
+    '''
+    def __init__(self, count_threads: int = -1, memory: int = 1):
+        '''
+        Create a new set of Docker constraints.
+        memory is measured in MB.
+        '''
+
+    @property
+    def cpu_threads(self) -> int:
+        '''
+        The maximum number of CPU threads the docker container can have.
+        -1 means that the amount of threads is not set, in which case the
+        number of threads will be determined when scheduling the job.
+        '''
+
+    @cpu_threads.setter
+    def cpu_threads(self, count_threads: int) -> None:
+        '''
+        Set the number of CPU threads.
+        If the number of cpu threads is already set(i.e it is not equal to -1),
+        this will raise a RuntimeError.
+        '''
+
+    @property
+    def memory(self) -> int:
+        '''
+        The maximum amount of RAM in MB to allocate for this container.
+        '''
